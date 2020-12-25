c95b85153c715264b2ea034ab388130e776db34a
刺客 <35906312+utomcat@users.noreply.github.com>
2020-12-25 16:13:01 +0800

    初始化第一个 Java-MongoDB 程序

---
+++ first_mongodb/src/main/java/com/ranyk/mongodb/demo/FirstMongoDbApplication.java
@@ -0,0 +1,16 @@
+package com.ranyk.mongodb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ranyk
+ */
+@SpringBootApplication
+public class FirstMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMongoDbApplication.class, args);
+    }
+
+}
